327cd6266fc711e2e6010f1c2f2ff4140ba9c6f8
Tim Guite <tim@magpieembedded.co.uk>
2024-04-20 18:06:06 +0100

    Feat: Generate template rust file for ESP 32

---
+++ esp32-c3-devkit-c-02-rust-bm/src/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
+
+extern crate alloc;
+use core::mem::MaybeUninit;
+
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+    init_heap();
+
+    esp_println::logger::init_logger_from_env();
+
+    loop {
+        log::info!("Hello world!");
+        delay.delay(500.millis());
+    }
+}
