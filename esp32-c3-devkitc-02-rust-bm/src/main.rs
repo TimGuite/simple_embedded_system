6a830b9a98e87860eb5ba3db60fd483b62ceb064
Tim Guite <tim@magpieembedded.co.uk>
2024-04-20 20:04:48 +0100

    Feat: Get blinky working on ESP32 in rust

--- esp32-c3-devkitc-02-rust-bm/src/main.rs
+++ esp32-c3-devkitc-02-rust-bm/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
+use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, gpio::IO, prelude::*};
 
 extern crate alloc;
 use core::mem::MaybeUninit;
@@ -24,6 +24,10 @@ fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
+    // Set up GPIO 19 for LED
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio19.into_push_pull_output();
+
     let clocks = ClockControl::max(system.clock_control).freeze();
     let delay = Delay::new(&clocks);
     init_heap();
@@ -31,7 +35,12 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     loop {
-        log::info!("Hello world!");
+        log::info!("LED On");
+        led.set_high();
+        delay.delay(500.millis());
+
+        log::info!("LED Off");
+        led.set_low();
         delay.delay(500.millis());
     }
 }
