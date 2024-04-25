7d4bcdafb6a7529b25acf703877678be8c79ef4a
Tim Guite <tim@magpieembedded.co.uk>
2024-04-25 14:56:07 +0100

    Feat: Check status

--- stm32_nucleo_f446re_zephyr/src/main.c
+++ stm32_nucleo_f446re_zephyr/src/main.c
@@ -22,53 +22,19 @@ int main(void)
 		return 0;
 	}
 
-	printf("Writing check value\n");
-	uint8_t check_bytes[1] = { 0x71 };
-	i2c_write(i2c_dev, check_bytes, 1, 0x38);
+	printf("Trigger Measurement\n");
+	uint8_t trigger_bytes[3] = { 0xAC, 0x33, 0x00 };
+	i2c_write(i2c_dev, trigger_bytes, 3, 0x38);
 
-	printf("Reading check value\n");
-	uint8_t rcv_bytes[1] = { 0 };
-	i2c_read(i2c_dev, rcv_bytes, 1, 0x38);
+	printf("Reading values\n");
+	uint8_t rcv_bytes[7] = { 0 };
+	i2c_read(i2c_dev, rcv_bytes, 7, 0x38);
 
 	printf("Received: %02X\n", rcv_bytes[0]);
+	if ((rcv_bytes[0] & 0x18) == 0x18)
+	{
+		printf("Status good\n");
+	}
 
-	// const struct device *const dev = DEVICE_DT_GET_ONE(aosong_dht20);
-	// int rc;
-
-	// if (!device_is_ready(dev)) {
-	// 	printf("Device %s is not ready\n", dev->name);
-	// 	return 0;
-	// }
-
-	// printf("Device set up...\n");
-
-	// while (true) {
-	// 	struct sensor_value temp, hum;
-
-	// 	printf("Fetching sample...\n");
-
-	// 	rc = sensor_sample_fetch(dev);
-
-	// 	printf("Sample fetched...\n");
-
-	// 	if (rc == 0) {
-	// 		rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP,
-	// 					&temp);
-	// 	}
-	// 	if (rc == 0) {
-	// 		rc = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY,
-	// 					&hum);
-	// 	}
-	// 	if (rc != 0) {
-	// 		printf("DHT20: failed: %d\n", rc);
-	// 		break;
-	// 	}
-
-	// 	printf("DHT20: %.2f Cel ; %0.2f %%RH\n",
-	// 	       sensor_value_to_double(&temp),
-	// 	       sensor_value_to_double(&hum));
-
-	// 	k_sleep(K_MSEC(2000));
-	// }
 	return 0;
 }
