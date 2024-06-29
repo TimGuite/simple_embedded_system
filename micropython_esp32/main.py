e0a17919ac63867741a9999c4e17488be429350d
Tim Guite <tim@magpieembedded.co.uk>
2024-06-29 20:53:51 +0100

    refactor: Move DHT20 driver to separate module

--- micropython_esp32/main.py
+++ micropython_esp32/main.py
@@ -1,4 +1,5 @@
 # Start Wokwi simulation and run:
+# $ mpremote connect port:rfc2217://localhost:4000 cp driver_dht20.py :driver_dht20.py
 # $ mpremote connect port:rfc2217://localhost:4000 run main.py
 
 import utime
@@ -6,19 +7,7 @@ import sys
 import machine
 
 from machine import I2C
-
-class DHT20:
-    def __init__(self, i2c_bus):
-        self.i2c_bus = i2c_bus
-    
-    def read(self):
-        self.i2c_bus.writeto(0x38, bytes([0xAC, 0x33, 0x00]))
-        read_buffer = self.i2c_bus.readfrom(0x38, 7)
-
-        humidity = ((read_buffer[1] << 12) + (read_buffer[2] << 4) + (read_buffer[3] >> 4)) * 9.536743e-5
-        temperature = ((((read_buffer[3] & 0x0F) << 16) + (read_buffer[4] << 8) + read_buffer[5]) * 1.9073486e-4) - 50  
-
-        return (humidity, temperature)
+from driver_dht20 import DHT20
 
 if __name__ == "__main__":
     print("Hello, Wokwi!")
