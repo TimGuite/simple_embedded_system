6619528028e5a09e93bde61cbd743b64bf63fcdb
Tim Guite <tim@magpieembedded.co.uk>
2024-06-29 20:32:09 +0100

    refactor: Move DHT20 code to separate class

--- micropython_esp32/main.py
+++ micropython_esp32/main.py
@@ -7,20 +7,29 @@ import machine
 
 from machine import I2C
 
+class DHT20:
+    def __init__(self, i2c_bus):
+        self.i2c_bus = i2c_bus
+    
+    def read(self):
+        self.i2c_bus.writeto(0x38, bytes([0xAC, 0x33, 0x00]))
+        read_buffer = self.i2c_bus.readfrom(0x38, 7)
+
+        humidity = ((read_buffer[1] << 12) + (read_buffer[2] << 4) + (read_buffer[3] >> 4)) * 9.536743e-5
+        temperature = ((((read_buffer[3] & 0x0F) << 16) + (read_buffer[4] << 8) + read_buffer[5]) * 1.9073486e-4) - 50  
+
+        return (humidity, temperature)
+
 if __name__ == "__main__":
     print("Hello, Wokwi!")
     board = sys.implementation._machine.split(' with')[0]
     print(f"Running on {board} ({sys.platform}) at {machine.freq() / 1000000} MHz")
 
-    i2c = I2C(0)
+    sensor = DHT20(I2C(0))
 
     counter = 0
     while True:
-        i2c.writeto(0x38, bytes([0xAC, 0x33, 0x00]))
-        read_buffer = i2c.readfrom(0x38, 7)
-
-        humidity = ((read_buffer[1] << 12) + (read_buffer[2] << 4) + (read_buffer[3] >> 4)) * 9.536743e-5
-        temperature = ((((read_buffer[3] & 0x0F) << 16) + (read_buffer[4] << 8) + read_buffer[5]) * 1.9073486e-4) - 50
+        humidity, temperature = sensor.read()
 
         print(f"Temperature: {temperature:.2f} degC, Humidity: {humidity:.2f} %")
 
